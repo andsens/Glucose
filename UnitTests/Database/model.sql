cb27aa385f8616458897d5d3c9c70f2dc2e265d7
Anders Ingemann <anders@ingemann.de>
2009-11-24 22:36:43 +0000

    Added data

--- UnitTests/Database/model.sql
+++ UnitTests/Database/model.sql
@@ -2,15 +2,13 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `model_unit_tests` ;
-USE `model_unit_tests`;
 
 -- -----------------------------------------------------
--- Table `model_unit_tests`.`countries`
+-- Table `countries`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `model_unit_tests`.`countries` ;
+DROP TABLE IF EXISTS `countries` ;
 
-CREATE  TABLE IF NOT EXISTS `model_unit_tests`.`countries` (
+CREATE  TABLE IF NOT EXISTS `countries` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(255) NOT NULL ,
   PRIMARY KEY (`id`) )
@@ -18,11 +16,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `model_unit_tests`.`cities`
+-- Table `cities`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `model_unit_tests`.`cities` ;
+DROP TABLE IF EXISTS `cities` ;
 
-CREATE  TABLE IF NOT EXISTS `model_unit_tests`.`cities` (
+CREATE  TABLE IF NOT EXISTS `cities` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `country` INT NOT NULL ,
   `name` VARCHAR(255) NOT NULL ,
@@ -32,18 +30,18 @@ CREATE  TABLE IF NOT EXISTS `model_unit_tests`.`cities` (
   UNIQUE INDEX `UNIQUE_cities__country__postal_code` (`country` ASC, `postal_code` ASC) ,
   CONSTRAINT `FK_cities__country`
     FOREIGN KEY (`country` )
-    REFERENCES `model_unit_tests`.`countries` (`id` )
+    REFERENCES `countries` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `model_unit_tests`.`people`
+-- Table `people`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `model_unit_tests`.`people` ;
+DROP TABLE IF EXISTS `people` ;
 
-CREATE  TABLE IF NOT EXISTS `model_unit_tests`.`people` (
+CREATE  TABLE IF NOT EXISTS `people` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `first_name` VARCHAR(255) NOT NULL ,
   `last_name` VARCHAR(255) NOT NULL ,
@@ -55,7 +53,7 @@ CREATE  TABLE IF NOT EXISTS `model_unit_tests`.`people` (
   UNIQUE INDEX `UNIQUE_customers__email` (`email` ASC) ,
   CONSTRAINT `FK_customers__city`
     FOREIGN KEY (`city` )
-    REFERENCES `model_unit_tests`.`cities` (`id` )
+    REFERENCES `cities` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -67,10 +65,9 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
--- Data for table `model_unit_tests`.`countries`
+-- Data for table `countries`
 -- -----------------------------------------------------
 SET AUTOCOMMIT=0;
-USE `model_unit_tests`;
 INSERT INTO `countries` (`id`, `name`) VALUES (1, 'Germany');
 INSERT INTO `countries` (`id`, `name`) VALUES (2, 'Denmark');
 INSERT INTO `countries` (`id`, `name`) VALUES (3, 'Norway');
@@ -78,25 +75,25 @@ INSERT INTO `countries` (`id`, `name`) VALUES (4, 'Sweden');
 INSERT INTO `countries` (`id`, `name`) VALUES (5, 'Finland');
 INSERT INTO `countries` (`id`, `name`) VALUES (6, 'Netherlands');
 INSERT INTO `countries` (`id`, `name`) VALUES (7, 'Belgium');
+INSERT INTO `countries` (`id`, `name`) VALUES (8, 'United States of America');
 
 COMMIT;
 
 -- -----------------------------------------------------
--- Data for table `model_unit_tests`.`cities`
+-- Data for table `cities`
 -- -----------------------------------------------------
 SET AUTOCOMMIT=0;
-USE `model_unit_tests`;
 INSERT INTO `cities` (`id`, `country`, `name`, `postal_code`) VALUES (1, 2, 'Århus', 8000);
 INSERT INTO `cities` (`id`, `country`, `name`, `postal_code`) VALUES (2, 1, 'Hamburg', 20095);
 INSERT INTO `cities` (`id`, `country`, `name`, `postal_code`) VALUES (3, 5, 'Helsinki', 00);
+INSERT INTO `cities` (`id`, `country`, `name`, `postal_code`) VALUES (4, 8, 'Andeby', 0);
 
 COMMIT;
 
 -- -----------------------------------------------------
--- Data for table `model_unit_tests`.`people`
+-- Data for table `people`
 -- -----------------------------------------------------
 SET AUTOCOMMIT=0;
-USE `model_unit_tests`;
 INSERT INTO `people` (`id`, `first_name`, `last_name`, `email`, `address`, `city`) VALUES (1, 'Anders', 'Ingemann', 'anders@ingemann.de', 'Vej 13', 1);
 
 COMMIT;
