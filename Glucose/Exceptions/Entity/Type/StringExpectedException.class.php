90ca562e41d45e5e9dc3ee5bb1238a6768b4a3ab
Anders Ingemann <anders@ingemann.de>
2010-07-28 13:10:40 +0000

    Refactored the exceptions

--- Glucose/Exceptions/Entity/Type/StringExpectedException.class.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace Glucose\Exceptions\Entity\Type;
-class StringExpectedException extends \InvalidArgumentException implements TypeException {
-	
-}
---
+++ Glucose/Exceptions/User/Type/StringExpectedException.class.php
@@ -0,0 +1,5 @@
+<?php
+namespace Glucose\Exceptions\User\Type;
+class StringExpectedException extends \InvalidArgumentException implements \Glucose\Exceptions\User\TypeMismatchException {
+	
+}
